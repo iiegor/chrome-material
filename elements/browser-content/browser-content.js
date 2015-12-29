fc33aea1b4fdbdba2c3999040a03c919768edc58
iiegor <iegormundo@hotmail.com>
2015-12-29 19:43:54 +0100

    Handle some webview events and add fullscreen support

--- elements/browser-content/browser-content.js
+++ elements/browser-content/browser-content.js
@@ -23,6 +23,7 @@
       this.tabs = [this._createTab('Google', 'http://google.com')];
 
       this._setSelected(0);
+      setImmediate(this._bindView.bind(this));
     },
 
     _createTab(title, href) {
@@ -53,8 +54,9 @@
 
       this.push('tabs', tab);
 
-      // set as selected
+      // set as selected and bind view events
       this._setSelected(this.tabs.length - 1);
+      this._bindView();
     },
 
     handleClose(e) {
@@ -71,8 +73,50 @@
       this._setSelected(index - 1);
     },
 
+    _bindView() {
+      this.currentView.addEventListener('did-start-loading', e => {
+        this.set(`tabs.${this.activeTab}.isLoading`, true);
+      });
+
+      this.currentView.addEventListener('did-stop-loading', e => {
+        this.set(`tabs.${this.activeTab}.isLoading`, false);
+      });
+
+      this.currentView.addEventListener('did-fail-load', e => {
+        // TODO: Catch the rest of error codes :P
+        switch (e.errorCode) {
+          // ERR_NAME_NOT_RESOLVED
+          case -105:
+            this.currentView.setAttribute('src', 'internal://pages/error.html');
+            break;
+        }
+      });
+
+      this.currentView.addEventListener('page-title-updated', e => {
+        this.set(`tabs.${this.activeTab}.title`, e.title);
+
+        document.title = e.title;
+      });
+
+      this.currentView.addEventListener('page-favicon-updated', e => {
+        this.set(`tabs.${this.activeTab}.favicon`, e.favicons[0]);
+      });
+
+      this.currentView.addEventListener('enter-html-full-screen', e => {
+        document.body.classList.add('fullscreen');
+      });
+
+      this.currentView.addEventListener('leave-html-full-screen', e => {
+        document.body.classList.remove('fullscreen');
+      });
+    },
+
     get currentView() {
       return this.$$('div.iron-selected > #view');
+    },
+
+    get currentTab() {
+      return this.tabs[this.activeTab];
     }
   });
 })();
