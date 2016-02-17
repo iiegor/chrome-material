2a8acbda61fbd2874c37a76ae20b00e9e0dbd7b3
iiegor <iegormundo@hotmail.com>
2016-02-17 17:27:08 +0100

    Update browser-header element

--- elements/browser-header/browser-header.js
+++ elements/browser-header/browser-header.js
@@ -2,6 +2,14 @@
 
 (() => {
   const url = require('url');
+  const remote = require('electron').remote;
+  const Menu = remote.Menu;
+  const MenuItem = remote.MenuItem;
+
+  var menu = new Menu();
+  menu.append(new MenuItem({ label: 'Settings', click: function() { console.log('item 1 clicked'); } }));
+  menu.append(new MenuItem({ type: 'separator' }));
+  menu.append(new MenuItem({ label: 'Exit' }));
 
   Polymer({
     is: 'browser-header',
@@ -21,6 +29,10 @@
       } else {
         el.classList.add('marked');
       }
+    },
+
+    handleMore(e) {
+      menu.popup(remote.getCurrentWindow(), e.currentTarget.offsetLeft - 55, e.currentTarget.offsetTop + e.currentTarget.clientHeight + 25);
     },
 
     handleNavigate(e) {
